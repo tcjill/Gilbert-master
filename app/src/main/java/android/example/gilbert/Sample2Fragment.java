0404a296568d6aaa356e5fbe95ce3c76d0efb7bd
jill <jgundaker@gmail.com>
2019-05-26 06:39:09 -0700

    sample fragments Deleted extra miwok from fragments

---
+++ app/src/main/java/android/example/gilbert/Sample2Fragment.java
@@ -0,0 +1,30 @@
+package android.example.gilbert;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Sample2Fragment extends Fragment {
+
+
+    public Sample2Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
