618687ee71a080a3a34011efdda7e42203017742
jill <jgundaker@gmail.com>
2019-05-26 07:01:55 -0700

    removed tourAdapter miwok related code

--- app/src/main/java/android/example/gilbert/TourAdapter.java
+++ app/src/main/java/android/example/gilbert/TourAdapter.java
@@ -36,33 +36,7 @@ public class TourAdapter extends ArrayAdapter<Tour>  {
         // Get the {@link Word} object located at this position in the list
         Tour currentWord = getItem(position);
 
-        // Find the TextView in the list_item.xml layout with the ID miwok_text_view.
-        //TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
-        // Get the Miwok translation from the currentWord object and set this text on
-        // the Miwok TextView.
-       // miwokTextView.setText(currentWord.getMiwokTranslation());
 
-        // Find the TextView in the list_item.xml layout with the ID default_text_view.
-       // TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
-        // Get the default translation from the currentWord object and set this text on
-        // the default TextView.
-        //defaultTextView.setText(currentWord.getDefaultTranslation());
-
-        // Find the ImageView in the list_item.xml layout with the ID image.
-        //ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
-        // Check if an image is provided for this word or not
-       // if (currentWord.hasImage()) {
-            // If an image is available, display the provided image based on the resource ID
-            //imageView.setImageResource(currentWord.getImageResourceId());
-            // Make sure the view is visible
-          //  imageView.setVisibility(View.VISIBLE);
-       // } else {
-            // Otherwise hide the ImageView (set visibility to GONE)
-         //   imageView.setVisibility(View.GONE);
-       // }
-
-        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
-        // the ListView.
         return listItemView;
     }
 }
