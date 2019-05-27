d377f69eaa10ffaceb854f593ae6f045a7bb8eaa
jill <jgundaker@gmail.com>
2019-05-27 08:58:54 -0700

    got images

--- app/src/main/java/android/example/gilbert/TourAdapter.java
+++ app/src/main/java/android/example/gilbert/TourAdapter.java
@@ -1,6 +1,7 @@
 package android.example.gilbert;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,7 +43,29 @@ public class TourAdapter extends ArrayAdapter<Tour>  {
 
 
         //ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
-       // imageView setImageResource(currentWord.getImageResourceId());
+        //imageView setImageResource(currentWord.getImageResourceId());
+        //Find the ImageView in the list_item.xml layout with the ID image.
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        // Check if an image is provided for this word or not
+        if (currentWord.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentWord.getImageResourceId());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+       // int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        //textContainer.setBackgroundColor(color);
+
+        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
+        // the ListView.
 
 
 
